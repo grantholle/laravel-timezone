2d855cf9f1715dd16aebca028b9dffdfaa3e0e0f
Grant Holle <hollegrant@gmail.com>
2023-02-17 11:35:11 +0400

    Add facade doc blocks

--- src/Facades/Timezone.php
+++ src/Facades/Timezone.php
@@ -2,9 +2,20 @@
 
 namespace GrantHolle\Timezone\Facades;
 
+use Carbon\Carbon;
+use Carbon\CarbonImmutable;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static string getCurrentTimezone()
+ * @method static string|Collection timezones(?string $timezone = null)
+ * @method static string|CarbonImmutable toLocal(null|Carbon|CarbonImmutable $date, string $format = null)
+ * @method static string toLocalFormatted(null|Carbon|CarbonImmutable $date, string $format = null)
+ * @method static CarbonImmutable fromLocal(mixed $date)
+ * @method static CarbonImmutable today()
+ * @method static CarbonImmutable now()
+ *
  * @see \GrantHolle\Timezone\Timezone
  */
 class Timezone extends Facade
